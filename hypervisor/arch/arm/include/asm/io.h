c17bf6666595e4ebe6227d30d946f90bc8305ed3
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2014-06-11 17:09:43 +0100

    arm: add IO helpers
    Linux's arch/arm/include/io.h uses inline asm to make sure that the
    compiler doesn't generate any register write-back, which are extremely
    difficult to emulate.
    It shouldn't be a requirement in Jailhouse, since arm doesn't currently
    support nested hypervisors.
    The `volatile' attribute should thus be sufficient to ensure that the
    compiler doesn't optimize these away.
    
    This patch still separates the IO accessors from the core, to facilitate
    a possible replacement with inline assembly.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>

---
+++ hypervisor/arch/arm/include/asm/io.h
@@ -0,0 +1,63 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _JAILHOUSE_ASM_IO_H
+#define _JAILHOUSE_ASM_IO_H
+
+#include <asm/types.h>
+
+#ifndef __ASSEMBLY__
+
+static inline void writeb_relaxed(u8 val, volatile void *addr)
+{
+	*(volatile u8 *)addr = val;
+}
+
+static inline void writew_relaxed(u16 val, volatile void *addr)
+{
+	*(volatile u16 *)addr = val;
+}
+
+static inline void writel_relaxed(u32 val, volatile void *addr)
+{
+	*(volatile u32 *)addr = val;
+}
+
+static inline void writeq_relaxed(u64 val, volatile void *addr)
+{
+	/* Warning: no guarantee of atomicity */
+	*(volatile u64 *)addr = val;
+}
+
+static inline u8 readb_relaxed(volatile void *addr)
+{
+	 return *(volatile u8 *)addr;
+}
+
+static inline u16 readw_relaxed(volatile void *addr)
+{
+	 return *(volatile u16 *)addr;
+}
+
+static inline u32 readl_relaxed(volatile void *addr)
+{
+	 return *(volatile u32 *)addr;
+}
+
+static inline u64 readq_relaxed(volatile void *addr)
+{
+	/* Warning: no guarantee of atomicity */
+	return *(volatile u64 *)addr;
+}
+
+#endif /* !__ASSEMBLY__ */
+#endif /* !_JAILHOUSE_ASM_IO_H */
