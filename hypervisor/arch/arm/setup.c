cb2fe25ab6eddfc1fc5d6b02cd83a765f91fa324
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2014-06-12 17:16:28 +0100

    arm: implement the paging callbacks
    This patch adds the ability to implement EL2 stage-1 and EL1 stage-2
    MMU, using the struct paging defined by the core.
    In the future, it may be useful to separate the stage1 and stage2
    functions, like the x86 implementation, in order to use less translation
    levels for the IPA->PA translation. For the moment, we keep the
    hv_paging = arm_paging definition.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>

--- hypervisor/arch/arm/setup.c
+++ hypervisor/arch/arm/setup.c
@@ -86,9 +86,5 @@ void arch_shutdown(void) {}
 unsigned long arch_page_map_gphys2phys(struct per_cpu *cpu_data,
 				       unsigned long gphys)
 { return INVALID_PHYS_ADDR; }
-void arch_paging_init(void) { }
-
-const struct paging arm_paging[1];
-
 void arch_panic_stop(struct per_cpu *cpu_data) {__builtin_unreachable();}
 void arch_panic_halt(struct per_cpu *cpu_data) {}
